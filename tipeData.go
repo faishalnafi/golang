f133821b1751a3beeb4d8205718bf3ae666c7f2c
Faishal Nafi <faishalnafi50@gmail.com>
2024-03-10 00:33:04 +0700

    macam macam tipe data

---
+++ tipeData.go
@@ -0,0 +1,16 @@
+//Program belajar GO-Lang
+//09 Maret 2024
+package main
+import "fmt"
+
+func main() {
+
+   //tipe data angka
+  fmt.Println("satu = ",1);
+  fmt.Println("dua = ",2);
+  fmt.Println("tiga koma lima = ", 3.5);
+
+  //tipe data boolean
+  fmt.Println("benar = ", true)
+  fmt.Println("salah = ", false)
+}
