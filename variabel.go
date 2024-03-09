2d22064ba70b584a0e56f09f6736ae31ebad2037
Faishal Nafi <faishalnafi50@gmail.com>
2024-03-10 01:03:58 +0700

    variabel

---
+++ variabel.go
@@ -0,0 +1,40 @@
+//Program belajar GO-Lang
+//09 Maret 2024
+package main
+import "fmt"
+
+func main() {
+	var nama string
+
+	nama = "Faishal Nafi' Rabbani"
+	fmt.Println(nama)
+
+	nama = "Faishal Nafi'"
+	fmt.Println(nama)
+
+	var panggilan = "afii"
+	fmt.Println(panggilan)
+	
+	var age = 20
+	fmt.Println(age)
+
+	var ages int8 = 20
+	fmt.Println(ages)
+
+	var umur int
+	umur = 20
+	fmt.Println(umur)
+
+	country := "Indo"
+	fmt.Println(country)
+	country = "Amrik"
+	fmt.Println(country)
+
+	var (
+		firstname = "Afii"
+		lastname = "Napikk"
+	)
+
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+}
