f6e44e6f3490e6d190e0dda05553bd4b10af6468
faishalnafi <faishalnafi50@gmail.com>
2024-04-19 19:02:28 +0700

    konsep pointer, replace value dengan alamat

--- point-er.go
+++ point-er.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var tinggi int = 90;
-	fmt.Println("value tinggi= ",tinggi)
-	fmt.Println("value tinggi= ",&tinggi)
-	fmt.Println("value tinggi= ",*&tinggi)
+	var tinggi int = 90
+	fmt.Println("value tinggi : ", tinggi)
+	fmt.Println("alamat tinggi : ", &tinggi)
+	fmt.Println("dereference tinggi : ", *&tinggi)
+	fmt.Println()
 
-	
+	var alamat *int
+	alamat = &tinggi
+	fmt.Println("isi alamat : ", alamat)
+	fmt.Println("value dari var alamat :", *alamat)
+	fmt.Println()
+
+	*alamat = 1000
+	fmt.Println("value tinggi :", tinggi)
 }
